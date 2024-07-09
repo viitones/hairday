a755a0dbc4ddc24268c2d4b14358eba8df96237a
viitones <vitonesmartins@gmail.com>
2024-07-09 16:26:45 -0300

    config use css with webpack

---
+++ src/main.js
@@ -0,0 +1,7 @@
+"use strict"
+
+//imports css
+
+import "./styles/global.css";
+import "./styles/form.css";
+import "./styles/schedule.css";
--- webpack.config.js
+++ webpack.config.js
@@ -25,5 +25,14 @@ module.exports = {
       template: path.resolve(__dirname, "index.html"),
       favicon: path.resolve("src", "assets", "scissors.svg")
     })
-  ]
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ "style-loader", "css-loader" ],
+      },
+    ]
+  }
 }
