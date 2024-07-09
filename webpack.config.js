cef0842f28207c849d16a46757ce25c15888706c
viitones <vitonesmartins@gmail.com>
2024-07-09 19:35:35 -0300

    add compiler babel and config

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,18 @@ module.exports = {
         test: /\.css$/,
         use: [ "style-loader", "css-loader" ],
       },
+      {
+        test: /\.(?:js|mjs|cjs)$/, 
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              ["@babel/preset-env", {targets: "defaults"}]
+            ]
+          }
+        }
+      }
     ]
   }
 }
