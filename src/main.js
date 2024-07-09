83c2e25061dfe1cd319c407aa44b714513186d03
viitones <vitonesmartins@gmail.com>
2024-07-09 19:41:54 -0300

    add dayjs lib for dates

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 "use strict"
 
-//imports css
+//importação do config do day.js
+import "./libs/dayjs"
 
+//imports css
 import "./styles/global.css";
 import "./styles/form.css";
 import "./styles/schedule.css";
+
